e364c0a2834b1abffbe2662514f2a65e6cf061ff
gab246 <119339137+gab246@users.noreply.github.com>
2023-08-24 18:49:39 +0200

    updated log in form

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -11,11 +12,13 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class UserLoginFormComponent implements OnInit {
 
   @Input() loginData = { Username: '', Password: '' };
+  
 
 constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar) { }
+    public snackBar: MatSnackBar,
+    private router: Router) {}
 
 ngOnInit(): void {
 }
@@ -25,6 +28,7 @@ userLogin(): void {
     localStorage.setItem('user', JSON.stringify(result.user));
     localStorage.setItem('token', result.token)
     this.dialogRef.close(); 
+    this.router.navigate(['Movies']);
     console.log(result)
     this.snackBar.open(result, 'OK', {
       duration: 2000
